9ee07ec9568ec994436f91a8b36e6892170c7a4c
Zindae <thomasvp@kth.se>
2016-02-15 10:57:33 +0100

    Added
    Added files

---
+++ js/app.js
@@ -0,0 +1,42 @@
+$(function() {
+	//We instantiate our model
+	var model = new DinnerModel();
+	
+	//And create the needed controllers and views
+	var exampleView = new ExampleView($("#exampleView"));
+
+	
+
+	// $(document).ready(function(){        
+	// 	// ADD SOME .css("display", "");
+ //        $('#button1, #button2, #button3, #button4, #button5').click(function() {        
+ //             switch(this.id) {
+ //             	case 'button1'	:
+ //             		$('.view1-and-view2, .view3-and-view4').hide('fast');
+ //             		$('.home-page-start').show('fast');
+ //             		// $('.view3-and-view4').hide('fast');
+ //             		// $('home-page-start').css(	)
+ //             		break;
+ //             	case 'button2':
+ //             		$('.home-page-start, .ingredients-and-preparation, .view3-and-view4').hide('fast');
+ //             		$('.view1-and-view2 ').show('fast');
+ //             		break;
+ //             	case 'button3':
+ //             		$('.home-page-start, .select-dishes, .view3-and-view4').hide('fast');
+ //             		$('.view1-and-view2 ').show('fast');
+ //             		break;
+ //             	case 'button4':
+ //             		$('.home-page-start, .view1-and-view2, .dinner-preparation').hide('fast');
+ //             		$('.view3-and-view4 ').show('fast');
+ //             		break;
+ //             	case 'button5':
+ //             		$('.home-page-start, .view1-and-view2, .dinner-overview').hide('fast');
+ //             		$('.view3-and-view4 ').show('fast');
+ //             		break;
+	// 		}
+ //        });
+ //    });
+
+
+
+});
