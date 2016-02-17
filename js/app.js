b3c785c92d4ed6f33911608269af75b95ebfdfb9
molleweide <hjalmar.jakobsson@telia.com>
2016-02-17 11:35:41 +0100

    fixed a lot of stuff

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+var model
 $(document).ready(function(){        
 	
 	// Get DinnerModel function
-	var model = new DinnerModel();
+	model = new DinnerModel();
 	
 
 	// Hide / Show pages
@@ -45,29 +46,29 @@ $(document).ready(function(){
 	// Function to detect click on single food course main1view1. On click, show main1view2.
 	// Problem: Need either 1 view for each course or generate new views each time with clicked data.
 	
-	$('.main1view1').delegate('div','click',function() {
-		$(".main1view2").html()
+	// $('.main1view1').delegate('div','click',function() {
+	// 	$(".main1view2").html()
 		
-		$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
-		$('.main1').show(); 
-		$('.start-page, .main1view1, .main2').hide();
+	// 	$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+	// 	$('.main1').show(); 
+	// 	$('.start-page, .main1view1, .main2').hide();
 		
-		//var selected_html = ed.selection.getContent();
-		//var $name = $elem.filter('name');
-		//console.log($name);
-		var temp1 = $('#3').attr('id');
-		console.log(temp1);
-		var name = allRecipes[temp1];
+	// 	//var selected_html = ed.selection.getContent();
+	// 	//var $name = $elem.filter('name');
+	// 	//console.log($name);
+	// 	var temp1 = $('#3').attr('id');
+	// 	console.log(temp1);
+	// 	var name = allRecipes[temp1];
 		
-		$(".main1view2").append('<button id="back">Back to Dishes</button>')
-		$(".main1view2").append('<button id="confirmDish">Confirm Dish</button>')
-		$(".main1view2").append('<div id="'+temp1+'" class="col-xs-2"><img class="icon" src="./images/'+name.image+'"><p><b>'+name.name+'</b></p><p>'+name.description+'</p></div>');
+	// 	$(".main1view2").append('<button id="back">Back to Dishes</button>')
+	// 	$(".main1view2").append('<button id="confirmDish">Confirm Dish</button>')
+	// 	$(".main1view2").append('<div id="'+temp1+'" class="col-xs-2"><img class="icon" src="./images/'+name.image+'"><p><b>'+name.name+'</b></p><p>'+name.description+'</p></div>');
 
-		// $( "div" ).text( temp );
-		//console.log($('main1view2').get( 0 ));
-		//$(".main1view2").append('<div class="col-xs-2"><img class="icon" src="./images/'+name.image+'"><p><b>'+name.name+'</b></p><p>'+name.description+'</p></div>');
+	// 	// $( "div" ).text( temp );
+	// 	//console.log($('main1view2').get( 0  ));
+	// 	//$(".main1view2").append('<div class="col-xs-2"><img class="icon" src="./images/'+name.image+'"><p><b>'+name.name+'</b></p><p>'+name.description+'</p></div>');
 	
-	});
+	// });
 		
 		
 		
@@ -75,7 +76,7 @@ $(document).ready(function(){
 	
 	// All recipes 
 	var allRecipes = model.getAll();
-	console.log(allRecipes);
+	// console.log(allRecipes);
 	
 	// Length of the array for the FOR loop
 	var len = allRecipes.length;
@@ -86,6 +87,8 @@ $(document).ready(function(){
 		
 		// Gets specific recipe
 		var outputDish = allRecipes[i];
+		// console.log(allRecipes);
+		// console.log(outputDish);
 	
 		//Creates DIV with recipe info.
 
@@ -93,7 +96,7 @@ $(document).ready(function(){
 
 
 		//$(".main1view1").append('<a href="#" data-toggle="popover" title="'+name.name+'" data-content="'+name.description+'">123</a>');
-		$(".main1view1").append('<div id="'+i+'" class="col-xs-2"><img class="icon" src="./images/'+name.image+'"><p><b>'+name.name+'</b></p></div>');
+		// $(".main1view1").append('<div id="'+i+'" class="col-xs-2"><img class="icon" src="./images/'+outputDish.image+'"><p><b>'+outputDish.name+'</b></p></div>');
 
 	};
 
@@ -103,31 +106,30 @@ $(document).ready(function(){
 
 
 	$('.main1view1dishes').click(function(){
-	
+		
 		var dishID = $(this).attr('id');
-		// click div w/.main1view1dishes
-		// => display main1view2
-		// $(.main1view2) append
 
-		// click div.
+		// console.log(dishID);
 
-		// append append (allRecipes(div#id)) to main1view2
-		$(".main1view2").append('<div class=" col-xs--6  main1-view2-dish "> \
-									<img class="icon" src="./images/'+allRecipes[dishID.image]+'">\
-									<p>'+allRecipes[dishID.description]+'</p>\	
-									<button>back to Select Dishes</button>\
-								</div>\
-								<div class=" col-xs--6  main1-view2-ingr ">\
-									<p>'+allRecipes[dishID.ingredients]+'</p>\	
-									<button>Confirm Dish</button>\
-								</div>\
-								<div class=" col-xs--6  main1-view2-prep ">\
-								\
-								</div>');
+		// var allRecipes = model.getAll();
+		
+		// console.log(allRecipes);
+
+		
+		// var oneRecipe = allRecipes[dishID ]
+		var oneRecipe = model.getDish(dishID);
+		
+		// console.log("This is dish: " + oneRecipe.name);
+
+
+		$(".main1view2").append('<div class=" col-xs-6  main1-view2-dish "><h2>' + oneRecipe.name + '</h2><img class="icon" src="./images/' + oneRecipe.image + '"><h4>' + oneRecipe.description + '</h4><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.</p><button>back to Select Dishes</button></div>');
+		$(".main1view2").append('<div class=" col-xs-6  main1-view2-ingr "><h3>' + oneRecipe.ingredients + '</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.</p><button>Confirm Dish</button></div>');
+		$(".main1view2").append('<div class=" col-xs-6  main1-view2-prep "><h3>'+oneRecipe.type +'</h3><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut.</p></div>');
 			
 		$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
 		$('.main1').show(); 
-		$('.start-page, .main1view2, .main2').hide();
+ 		$('.start-page, .main1view1, .main2').hide();
+	});
 	
 
 
@@ -208,48 +210,48 @@ $(document).ready(function(){
 
 // $(document).ready(function(){        
 		
-		// // ==> SHOW & hide used to display each 'page'
-		// // short css 'display' reset before each hide/show in order to prevent
-		// // conflicts since hide/show is abstracted under jquery.
+// 		// ==> SHOW & hide used to display each 'page'
+// 		// short css 'display' reset before each hide/show in order to prevent
+// 		// conflicts since hide/show is abstracted under jquery.
         
-        // $('#button1, #button2, #button3, #button4, #button5').click(function() {        
+//         $('#button1, #button2, #button3, #button4, #button5').click(function() {        
              
-             // switch(this.id) {
+//              switch(this.id) {
              	
-             	// // start-page
-             	// case 'button1'	: 
-             		// $('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
-             		// $('.start-page').show(); 
-             		// $('.main1, .main2').hide();
-             		// break; 
+//              	// start-page
+//              	case 'button1'	: 
+//              		$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+//              		$('.start-page').show(); 
+//              		$('.main1, .main2').hide();
+//              		break; 
              	
-             	// // main1view1
-             	// case 'button2':
-             		// $('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
-             		// $('.main1').show(); 
-             		// $('.start-page, .main1view2, .main2').hide();
-             		// break; 
+//              	// main1view1
+//              	case 'button2':
+//              		$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+//              		$('.main1').show(); 
+//              		$('.start-page, .main1view2, .main2').hide();
+//              		break; 
              		
-             	// // main1view2
-             	// case 'button3':
-					// $('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
-					// $('.main1').show(); 
-             		// $('.start-page, .main1view1, .main2').hide();
-             		// break; 
+//              	// main1view2
+//              	case 'button3':
+// 					$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+// 					$('.main1').show(); 
+//              		$('.start-page, .main1view1, .main2').hide();
+//              		break; 
              		
-             	// // main2view1
-             	// case 'button4':
-					// $('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
-					// $('.main2').show(); 
-             		// $('.start-page, .main2view2, .main1').hide();
-             		// break; 
+//              	// main2view1
+//              	case 'button4':
+// 					$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+// 					$('.main2').show(); 
+//              		$('.start-page, .main2view2, .main1').hide();
+//              		break; 
 
-             	// // main2view2
-             	// case 'button5':
-					// $('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
-					// $('.main2').show(); 
-             		// $('.start-page, .main2view1, .main1').hide();
-             		// break; 
-			// }
-        // });
-    // });
+//              	// main2view2
+//              	case 'button5':
+// 					$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+// 					$('.main2').show(); 
+//              		$('.start-page, .main2view1, .main1').hide();
+//              		break; 
+// 			}
+//         });
+//     });
