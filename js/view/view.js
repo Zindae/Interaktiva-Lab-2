7ffbd4e128046686a6d0c5f714071a819c9e26a9
molleweide <hjalmar.jakobsson@telia.com>
2016-02-17 11:36:58 +0100

    added view.js

---
+++ js/view/view.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){        
+		
+		// ==> SHOW & hide used to display each 'page'
+		// short css 'display' reset before each hide/show in order to prevent
+		// conflicts since hide/show is abstracted under jquery.
+        
+        $('#button1, #button2, #button3, #button4, #button5').click(function() {        
+             
+             switch(this.id) {
+             	
+             	// start-page
+             	case 'button1'	: 
+             		$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+             		$('.start-page').show(); 
+             		$('.main1, .main2').hide();
+             		break; 
+             	
+             	// main1view1
+             	case 'button2':
+             		$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+             		$('.main1').show(); 
+             		$('.start-page, .main1view2, .main2').hide();
+             		break; 
+             		
+             	// main1view2
+             	case 'button3':
+					$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+					$('.main1').show(); 
+             		$('.start-page, .main1view1, .main2').hide();
+             		break; 
+             		
+             	// main2view1
+             	case 'button4':
+					$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+					$('.main2').show(); 
+             		$('.start-page, .main2view2, .main1').hide();
+             		break; 
+
+             	// main2view2
+             	case 'button5':
+					$('.start-page, .main1, .main2, .main1view1, .main1view2, .main2view1, .main2view2').css('display', ''); 
+					$('.main2').show(); 
+             		$('.start-page, .main2view1, .main1').hide();
+             		break; 
+			}
+        });
+    });
